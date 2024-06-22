a5d4d7f9d4c566844c6c581efb67abf878fcc641
bigoldcat123 <a1456870251@163.com>
2024-06-22 11:13:41 +0800

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Ma_Shan_Zheng } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+})
+
+export const ma_shan_zheng = Ma_Shan_Zheng({
+    subsets: ['latin'],
+    weight: '400'
+}) 
