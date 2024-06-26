b51b797ca39531cab2c5f91403ea515ff751b4cf
bigoldcat123 <a1456870251@163.com>
2024-06-26 12:29:58 +0800

    e

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,6 +8,7 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { authenticate } from '../lib/actions';
+//@ts-ignore
 import {useActionState} from 'react'
 export default function LoginForm() {
   const [errorMessage, formAction, isPending] = useActionState(authenticate,undefined)
